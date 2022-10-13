03f5d1edb0e0b0824f87b81f0bdcb02dd5f31bb6
imightexist <94249457+imightexist@users.noreply.github.com>
2022-10-12 19:26:07 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const config = {
+  file:"./db.json"
+}
+
+let db = require(config.file);
+const fs = require('fs')
+
+db["abc"] = "funny";
+
+let newfile = file;
+if (newfile.startsWith("./")){
+  newfile.replace("./","");
+}
+fs.writeFileSync(newfile,JSON.stringify(db),"utf-8")
